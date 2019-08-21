449d6c725341e868f11e7c9371f79c4db954fa41
Administrator <Administrator@smart20114>
2019-08-21 10:05:04 +0900

    패키지 형식을 맞추서 입력해야 출력이 되고 map으로 선언하고 map에 name과 age를 담아 리턴 시킨다.

--- ksmart_layout1/src/main/java/kr/or/ksmart/ksmart_layout1/common/Common.java
+++ ksmart_layout1/src/main/java/kr/or/ksmart/ksmart_layout1/common/Common.java
@@ -1,5 +1,6 @@
-package kr.or.ksmart.ksmart_common;
+package kr.or.ksmart.ksmart_layout1.common;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Common {
 
-	@RequestMapping(value="/ajaxTest")
+	@RequestMapping(value="/ajaxTest", produces = "application/json")
 	public @ResponseBody Map<String,Object> ajaxTest(){
-		return null;
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("name", "홍길동");
+		map.put("age", "20");
+		
+		return map;
 	}
 }

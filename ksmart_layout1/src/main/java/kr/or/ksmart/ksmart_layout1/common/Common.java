a6ce0d2a330a14a7150440ea3b83be5df2531854
Administrator <Administrator@smart20114>
2019-08-21 11:54:45 +0900

    ajax를 map을 이용해서 화면에 보이기 하기 위해 jquery 사이트에서 예시를 가져와서 실행

--- ksmart_layout1/src/main/java/kr/or/ksmart/ksmart_layout1/common/Common.java
+++ ksmart_layout1/src/main/java/kr/or/ksmart/ksmart_layout1/common/Common.java
@@ -1,21 +1,39 @@
 package kr.or.ksmart.ksmart_layout1.common;
 
-import java.util.HashMap;
+
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import kr.or.ksmart.ksmart_layout1.service.BoardService;
 
 @Controller
 public class Common {
-
+	
+	@Autowired
+	private BoardService boardService;
+	
 	@RequestMapping(value="/ajaxTest", produces = "application/json")
-	public @ResponseBody Map<String,Object> ajaxTest(){
-		Map<String,Object> map = new HashMap<String,Object>();
-		map.put("name", "홍길동");
-		map.put("age", "20");
+	//디폴트값 1, 값이 안들어와도 된다.
+	public @ResponseBody Map<String,Object> ajaxTest(
+			@RequestParam(value="currentPage",
+			required = false, 
+			defaultValue = "1")int currentPage){
 		
-		return map;
+		/*
+		 * map<String,Object> map = boardService.addBoard(board); 
+		 * return map;
+		 */
+		
+		return boardService.boardList(currentPage);
+	}
+	
+	@RequestMapping("/ajaxCall")
+	public String ajaxCall() {
+		
+		return "ajax/ajaxCall";
 	}
 }
